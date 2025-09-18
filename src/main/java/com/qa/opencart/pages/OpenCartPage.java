c1c6a723a5f2b003489ee8fc9dbf61063b15f640
Mr.Perfect <amolgurav126@gmail.com>
2025-09-18 23:35:59 +0530

    practice

--- src/main/java/com/qa/opencart/pages/OpenCartPage.java
+++ src/main/java/com/qa/opencart/pages/OpenCartPage.java
@@ -2,4 +2,5 @@ package com.qa.opencart.pages;
 
 public class OpenCartPage {
 
+	int i = 10;
 }
--- src/test/java/com/qa/opencart/tests/OpenCarPageTest.java
+++ src/test/java/com/qa/opencart/tests/OpenCarPageTest.java
@@ -1,6 +1,12 @@
 package com.qa.opencart.tests;
 
+import org.testng.Assert;
+
+
 public class OpenCarPageTest {
 
 	String test = "git hub";
+	String test1 = "git hub";
+	
+	
 }
